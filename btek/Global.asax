4b3e6a59d085c3dc80a936c4db82c908e9959fc0
Tibi E <tiberiuegyed@yahoo.com>
2018-02-17 12:48:10 -0500

    Add project files.

---
+++ btek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="btek.MvcApplication" Language="C#" %>
---
+++ btek/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(btek.Startup))]
+namespace btek
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
