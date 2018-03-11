4244b5af31a6149dffc25f02b78d3cfee26c048a
Erasmo Medina <erasmo_medina93@hotmail.com>
2018-03-11 14:19:23 -0400

    added adPostModel migration

---
+++ btek/Models/AdPostModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace btek.Models
+{
+    public class AdPostModel
+    {
+        public string ClientName { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public double Price {get;set;}
+    }
+}
