911d090b3e26f7537b6136df2dcd55687d6ad94f
kennedeysok <o_sok@fanshaweonline.ca>
2018-03-27 14:23:30 -0400

    Added IsContractor in AccountVIewModel

--- btek/Models/AccountViewModels.cs
+++ btek/Models/AccountViewModels.cs
@@ -79,6 +79,8 @@ namespace btek.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+      
+        public bool IsContractor { get; set; }
     }
 
     public class ResetPasswordViewModel
